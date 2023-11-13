2e5d2bfbbb0132c4d6be0945cd15c7e77ec67d94
Youss0203 <youssefhalibi003@gmail.com>
2023-11-13 17:12:45 +0100

    Milestone 1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
-const userDistance = getElementbyid(distanza);
-const userAge = getElementbyid(age);
+const buttonElement = document.querySelector("button")
+
+buttonElement.addEventListener("click" ,function() {
+
+const userDistance = document.getElementById("distanza").value;
+const userAge = document.getElementById("age").value;
+
+
+let prezzoBiglietto = (userDistance * 0.1976);
+
+if(userAge > 18){
+    ((prezzoBiglietto * 17.65)/100)
+}else if(userAge <= 65){
+    ((prezzoBiglietto*53.27)/100)
+}
+
+console.log(parseFloat(prezzoBiglietto.toFixed(2)))
+
+})
